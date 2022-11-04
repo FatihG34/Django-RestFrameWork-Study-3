20da149f92e4d0e3bcc17bfd7598d7bc673d866c
FatihG34 <fthgnydn34@gmail.com>
2022-11-04 16:44:53 +0300

    serializers modified

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from profiles.models import Profile
+from profiles.models import Profile, ProfileStatus
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -11,3 +11,12 @@ def create_profile(sender, instance, created, **kwargs):
     print(instance.username, "__Created", created)
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Profile)
+def create_first_profile_message(sender, instance, created, **kwargs):
+    if created:
+        ProfileStatus.objects.create(
+            user_profile=instance,
+            status_massage=f"{instance.user.username} joined the club"
+        )
