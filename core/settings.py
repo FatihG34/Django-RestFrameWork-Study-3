05bf329775f5e403f5c9eb8c8e8544b3f8f6e345
FatihG34 <fthgnydn34@gmail.com>
2022-11-05 11:59:44 +0300

    fixed

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
+    "dj_rest_auth",
     "django_extensions",
     # my apps
     "profiles",
@@ -128,3 +130,10 @@ MEDIA_ROOT = 'uploads'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
